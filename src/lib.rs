72d983a57081814de3ec4009070d64235bcc119b
Martin Kavík <martin@kavik.cz>
2020-06-12 21:31:01 +0200

    Updated start and Seed

--- src/lib.rs
+++ src/lib.rs
@@ -157,5 +157,12 @@ fn view(model: &Model) -> Node<Msg> {
 
 #[wasm_bindgen(start)]
 pub fn start() {
-    App::start("app", init, update, view);
+    console_error_panic_hook::set_once();
+    
+    let root_element = document()
+        .get_elements_by_class_name("todoapp")
+        .item(0)
+        .expect("element with the class `todoapp`");
+
+    App::start(root_element, init, update, view);
 }
