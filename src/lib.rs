a078d9f38be19837c6c0004f0ab5dc3a0d258075
Martin Kavík <martin@kavik.cz>
2020-06-12 14:51:04 +0200

    `Msg` and `Model` updated

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,31 @@ fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
 //     Model
 // ------ ------
 
-type Model = i32;
+struct Model {
+    todos: BtreeMap<Ulid, Todo>,
+    new_todo_title: String,
+    selected_todo: Option<SelectedTodo>,
+    filter: Filter,
+    base_url: Url,
+}
+
+struct Todo {
+    id: Ulid,
+    title: String,
+    completed: bool,
+    element: ElRef<web_sys::HtmlElement>,
+}
+
+struct SelectedTodo {
+    id: Ulid,
+    title: String,
+}
+
+enum Filter {
+   All,
+   Active,
+   Completed,
+}
 
 // ------ ------
 //    Update
@@ -22,8 +46,23 @@ type Model = i32;
 
 #[derive(Copy, Clone)]
 enum Msg {
-    Increment,
-}
+    UrlChanged(subs::UrlChanged),
+    NewTodoTitleChanged(String),
+ 
+    // ------ Basic Todo operations ------
+    CreateTodo,
+    ToggleTodo(Ulid),
+    RemoveTodo(Ulid),
+    
+    // ------ Bulk operations ------
+    CheckOrUncheckAll,
+    ClearCompleted,
+    
+    // ------ Selection ------
+    SelectTodo(Option<Ulid>),
+    SelectedTodoTitleChanged(String),
+    SaveSelectedTodo,
+ }
 
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
